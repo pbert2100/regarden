8949f242e53959c2bc26e398b77e892f237a53f7
Bert <me@bert.com>
2022-05-31 17:53:31 -0300

    'manifest'

--- pages/_document.js
+++ pages/_document.js
@@ -13,8 +13,6 @@ class MyDocument extends Document {
             <meta name='apple-mobile-web-app-capable' content='yes' />
             <meta name='apple-mobile-web-app-status-bar-style' content='default' />
             <meta name='apple-mobile-web-app-title' content='Regarden' />
-            <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-            <meta http-equiv="content-language" content="en-US" />
             <meta name='format-detection' content='telephone=no' />
             <meta name='mobile-web-app-capable' content='yes' />
 
